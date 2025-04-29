58ba119da4f39f0fcf9c6d1f2800de04f9123273
moizansari2024 <moizansari672@gmail.com>
2025-04-29 01:10:35 -0700

    saylani login form

---
+++ script.js
@@ -0,0 +1,16 @@
+// const validEmail = "test@example.com";
+// const validPassword = "123456";
+
+// document.getElementById("loginButton").addEventListener("click", function () {
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
+//   const messageDiv = document.getElementById("message");
+
+//   if (email === validEmail && password === validPassword) {
+//     messageDiv.style.color = "green";
+//     messageDiv.textContent = "Login successful!";
+//   } else {
+//     messageDiv.style.color = "red";
+//     messageDiv.textContent = "Invalid email or password.";
+//   }
+// });
